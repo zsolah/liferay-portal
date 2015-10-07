2b532bbff313a9e1928b0d52ab1030c0d1851479
Zsolt Oláh <zsolt.olah@liferay.com>
2015-10-07 13:53:48 +0200

    LPS-52817 Revert "LPS-52280 SF"
    This reverts commit a760189fb47d00029f2f1e84746795c06a5feb30.

--- portal-impl/src/com/liferay/portal/image/ImageToolImpl.java
+++ portal-impl/src/com/liferay/portal/image/ImageToolImpl.java
@@ -664,7 +664,7 @@ public class ImageToolImpl implements ImageTool {
 		return _imageMagick;
 	}
 
-	protected void orderImageReaderSpis() {
+	protected void orderImageReaderServiceProviders() {
 		IIORegistry defaultIIORegistry = IIORegistry.getDefaultInstance();
 
 		ImageReaderSpi firstImageReaderSpi = null;
@@ -725,7 +725,7 @@ public class ImageToolImpl implements ImageTool {
 	private ImageToolImpl() {
 		ImageIO.setUseCache(PropsValues.IMAGE_IO_USE_DISK_CACHE);
 
-		orderImageReaderSpis();
+		orderImageReaderServiceProviders();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(ImageToolImpl.class);
