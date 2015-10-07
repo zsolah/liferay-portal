29db7c09776fc3eca770dbed6ab4e6bb2e6be437
Zsolt Oláh <zsolt.olah@liferay.com>
2015-10-07 14:13:26 +0200

    LPS-52817 Revert "LPS-52280 SF"
    This reverts commit 466c148a6a88797358519bc5a1034fbc57170263.

--- portal-impl/src/com/liferay/portal/image/ImageToolImpl.java
+++ portal-impl/src/com/liferay/portal/image/ImageToolImpl.java
@@ -511,7 +511,7 @@ public class ImageToolImpl implements ImageTool {
 			}
 
 			if (renderedImage == null) {
-				throw new IOException("Unsupported image type");
+				throw new IOException("Unsupported Image Type");
 			}
 		}
 		finally {
