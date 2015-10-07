2549bdf74764e31ed16a12085f9ad18d59e55212
Zsolt Oláh <zsolt.olah@liferay.com>
2015-10-07 14:21:03 +0200

    LPS-52817 Revert "LPS-52280 Update test"
    This reverts commit 76f6038a9b7673357225b7acb870a072b035af43.
    
    Conflicts:
    	portal-impl/test/integration/com/liferay/portal/image/ImageToolImplTest.java

--- portal-impl/test/integration/com/liferay/portal/image/ImageToolImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/image/ImageToolImplTest.java
@@ -23,9 +23,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
 import java.awt.image.DataBufferByte;
-import java.awt.image.DataBufferInt;
 import java.awt.image.RenderedImage;
 
 import java.io.File;
@@ -160,7 +158,10 @@ public class ImageToolImplTest {
 
 		Assert.assertNotNull(expectedImage);
 
-		DataBuffer expectedDataBuffer = expectedImage.getData().getDataBuffer();
+		DataBufferByte expectedDataBufferByte =
+			(DataBufferByte)expectedImage.getData().getDataBuffer();
+
+		byte[][] expectedData = expectedDataBufferByte.getBankData();
 
 		String expectedType = FileUtil.getExtension(fileName);
 
@@ -180,28 +181,17 @@ public class ImageToolImplTest {
 
 		Assert.assertNotNull(resultImage);
 
-		DataBuffer resultDataBuffer = resultImage.getData().getDataBuffer();
+		DataBufferByte resultDataBufferByte =
+			(DataBufferByte)resultImage.getData().getDataBuffer();
+
+		byte[][] resultData = resultDataBufferByte.getBankData();
 
 		String resultType = imageBag.getType();
 
 		Assert.assertTrue(
 			StringUtil.equalsIgnoreCase(expectedType, resultType));
-		Assert.assertTrue(
-			expectedDataBuffer instanceof DataBufferByte ||
-			expectedDataBuffer instanceof DataBufferInt);
 
-		if (expectedDataBuffer instanceof DataBufferByte) {
-			Assert.assertTrue(
-				Arrays.deepEquals(
-					((DataBufferByte)expectedDataBuffer).getBankData(),
-					((DataBufferByte)resultDataBuffer).getBankData()));
-		}
-		else if (expectedDataBuffer instanceof DataBufferInt) {
-			Assert.assertTrue(
-				Arrays.deepEquals(
-					((DataBufferInt)expectedDataBuffer).getBankData(),
-					((DataBufferInt)resultDataBuffer).getBankData()));
-		}
+		Assert.assertTrue(Arrays.deepEquals(expectedData, resultData));
 	}
 
 	protected void testCrop(
