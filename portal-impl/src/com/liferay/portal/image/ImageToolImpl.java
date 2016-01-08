658a58a5e37203621a0f9073f3816f504ff297ac
Zsolt Oláh <zsolt.olah@liferay.com>
2016-01-08 11:10:45 +0100

    LPS-52817 simplifying not needed code

--- portal-impl/src/com/liferay/portal/image/ImageToolImpl.java
+++ portal-impl/src/com/liferay/portal/image/ImageToolImpl.java
@@ -406,29 +406,25 @@ public class ImageToolImpl implements ImageTool {
 			Iterator<ImageReader> iterator = ImageIO.getImageReaders(
 				imageInputStream);
 
-			while (iterator.hasNext()) {
+			while ((renderedImage == null) && iterator.hasNext()) {
 				ImageReader imageReader = iterator.next();
 
 				imageReaders.offer(imageReader);
 
-				if (firstImageReader) {
-					imageReader.setInput(imageInputStream);
+				imageReader.setInput(imageInputStream);
 
-					int height = imageReader.getHeight(0);
-					int width = imageReader.getWidth(0);
+				int height = imageReader.getHeight(0);
+				int width = imageReader.getWidth(0);
 
-					if ((height > PropsValues.IMAGE_TOOL_IMAGE_MAX_HEIGHT) ||
-						(width > PropsValues.IMAGE_TOOL_IMAGE_MAX_WIDTH)) {
+				if ((height > PropsValues.IMAGE_TOOL_IMAGE_MAX_HEIGHT) ||
+					(width > PropsValues.IMAGE_TOOL_IMAGE_MAX_WIDTH)) {
 
-						throw new ImageResolutionException();
-					}
-
-					renderedImage = imageReader.read(0);
-
-					formatName = imageReader.getFormatName();
-
-					firstImageReader = false;
+					throw new ImageResolutionException();
 				}
+
+				renderedImage = imageReader.read(0);
+
+				formatName = imageReader.getFormatName();
 			}
 		}
 		finally {
